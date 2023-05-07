d3e79cc6eca615479dda5f53b232f4b00d3278b2
Paul <liyb.paul@gmail.com>
2023-05-07 12:30:47 +0800

    train set

--- 程序/test/cnn-vgg.py
+++ 程序/test/cnn-vgg.py
@@ -1,7 +1,68 @@
-import warnings
-warnings.filterwarnings('ignore', message='Unused import statement*')
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-import tensorflow as tf
-from tensorflow.keras.applications.vgg16 import VGG16model =VGG16(weights='imagenet')
+class Net(nn.Module):
 
-tf.
+    def __init__(self):
+        super(Net, self).__init__()
+        # 输入图像是单通道，conv1 kenrnel size=5*5，输出通道 6
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        # conv2 kernel size=5*5, 输出通道 16
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # 全连接层
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # max-pooling 采用一个 (2,2) 的滑动窗口
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # 核(kernel)大小是方形的话，可仅定义一个数字，如 (2,2) 用 2 即可
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        # 除了 batch 维度外的所有维度
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+net = Net()
+print(net)
+
+params = list(net.parameters())
+print('参数数量: ', len(params))
+# conv1.weight
+print('第一个参数大小: ', params[0].size())
+
+# 随机定义一个变量输入网络
+input = torch.randn(1, 1, 32, 32)
+out = net(input)
+print(out)
+
+# 清空所有参数的梯度缓存，然后计算随机梯度进行反向传播
+net.zero_grad()
+out.backward(torch.randn(1, 10))
+
+output = net(input)
+# 定义伪标签
+target = torch.randn(10)
+# 调整大小，使得和 output 一样的 size
+target = target.view(1, -1)
+criterion = nn.MSELoss()
+
+loss = criterion(output, target)
+print(loss)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
